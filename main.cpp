8a63e271a47b1f8c2df164270d22a9808c590b95
Neil <mr.neil.j@gmail.com>
2017-05-27 22:43:49 -0700

    Adjusted application to produce fewer warnings

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+/**************************************************************
+ * Author: Neil Johnson
+ *
+ * Date: 4.26.2017
+ *
+ * Description: This is the main function for the application
+ * where we are able to search / sort through a set of numbers
+ * that come from an external file(s).
+**************************************************************/
+
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
 #include "inputCheck.hpp"
 #include "sorting.hpp"
@@ -15,10 +24,7 @@ int main() {
 	std::cout << "you can search through multiple lists and see" << std::endl;
 	std::cout << "if the number you are looking for exists." 	 << std::endl;
 
-	// Ask the user for input on what value they want to look for
-	std::cout << "\nWhat number would you like to search for?: ";
-	// Save that value in a int variable
-	int numSearch = getInt();
+
 
 	//Create an array of all files that will be read
 	std::string fileSets[4] = {"controlSet.txt", "set1.txt", "set2.txt", "set3.txt"};
@@ -32,18 +38,26 @@ int main() {
 							"Binary Search (utilizing Quick Sort method)",
 							"Quit");
 
-	int searchResponse = -1;
+	int searchResponse;
 
 	do {
 		searchMenu.prompt();
 		searchResponse = searchMenu.getResponse();
 
 
+
+
 		if (searchResponse == 1) {
 			/*********************************************
 			 * LINEAR SEARCH
 			 *********************************************/
-			bool found = false;
+			// Ask the user for input on what value they want to look for
+			std::cout << "\nWhat number would you like to search for?: ";
+			// Save that value in a int variable
+			int numSearch = getInt();
+
+
+			bool found;
 
 			for (int i = 0; i < 4; i++) {
 
@@ -57,26 +71,33 @@ int main() {
 					std::cout << " not found." << std::endl;
 				}
 			}
-		} else if (searchResponse == 2 || searchResponse == 3) {
+		}
+		else if (searchResponse == 2 || searchResponse == 3) {
 			/*********************************************
 			 * QUICK SORT
 			 *********************************************/
 			std::ifstream inFile;
 			std::vector<int> groupNum;
+			std::string updatedFileSets[4];
 			int temp;
 
+			// Open up each file for the different number sets
 			for (int i = 0; i < 4; i++) {
 				inFile.open(fileSets[i]);
-
+				// Check to see if you can open the file
 				if (inFile.fail()) {
 					std::cout << "Could not open file, an error has occurred." << std::endl;
 					exit(1);
-				} else {
+				}
+				// Otherwise, read the number that is in the file into a usable vector for later
+				else {
 					while (inFile >> temp) {
 						groupNum.push_back(temp);
 					}
+					// Close the file
 					inFile.close();
 
+					// Run the quickSort method
 					quickSort(groupNum, 0, (groupNum.size() - 1));
 
 					//Ask the user where they would like to store the updated array / vector
@@ -84,38 +105,82 @@ int main() {
 					std::cout << "FileName: ";
 					std::string updatedFile = getString();
 
+					// Output the information to an external file
 					std::ofstream outFile;
 					outFile.open(updatedFile);
+
+					// Check to see if we were able to successfully open the file
 					if (outFile.fail()) {
 						std::cout << "Could not open file, and error has occurred" << std::endl;
 						exit(1);
-					} else {
-						for (int i = 0; i < groupNum.size(); i++) {
-							outFile << groupNum.at(i);
+					}
+					// Otherwise write the vector array to the external file
+					else {
+						for (int j = 0; j < groupNum.size(); j++) {
+							outFile << groupNum.at(j);
 							outFile << " ";
 						}
 						std::cout << "Successfully wrote updated array from file" << fileSets[i] << " to new file."
 								  << std::endl;
+						updatedFileSets[i] = updatedFile;
 					}
 
 				}
 			}
+			// Clear the vector out so it can be reused for the next file
+			groupNum.clear();
 			/*********************************************
 			 * BINARY SEARCH
 			 *********************************************/
-			 if (searchResponse == 3) {
+			if (searchResponse == 3) {
+				// Ask the user for input on what value they want to look for
+				std::cout << "\nWhat number would you like to search for?: ";
+				// Save that value in a int variable
+				int numSearch = getInt();
 
-			 }
+				bool found;
 
+				for (int i = 0; i < 4; i++) {
+					inFile.open(updatedFileSets[i]);
+
+					// Check to see if we could successfully open the file
+					if (inFile.fail()) {
+						std::cout << "Could not open file, an error has occurred." << std::endl;
+						exit(1);
+					}
+					// Otherwise begin reading the file into a vector
+					else {
+						while (inFile >> temp) {
+							groupNum.push_back(temp);
+						}
+						// Close the file
+						inFile.close();
+
+						// Run the binarySearch algorithm and return true or false to if the item was found
+						found = binarySearch(groupNum, numSearch, 0, groupNum.size() - 1);
+
+						// Print out the results of if we found it or not
+						std::cout << updatedFileSets[i] << ": target value " << "\"" << numSearch << "\"";
+
+						if (found) {
+							std::cout << " found." << std::endl;
+						} else {
+							std::cout << " not found." << std::endl;
+						}
+					}
+				}
+			}
 		}
+		// User has selected to leave the application, close accordingly
 		else if (searchResponse == 4) {
 			std::cout << "Thank you, have a great day!" << std::endl;
 		}
+		// Not a valid input, let the user know
 		else {
 			std::cout << "Not valid input, try again." << std::endl;
 		}
 	} while (searchResponse != 4);
 
-	
+
 	return 0;
 }
--- sorting.cpp
+++ sorting.cpp
@@ -1,31 +1,53 @@
-//
-// Created by Neil on 5/23/2017.
-//
+/**************************************************************
+ * Author: Neil Johnson
+ *
+ * Date: 4.26.2017
+ *
+ * Description: This is the sorting file for the various features
+ * inside of the main function.  In hear we are hosting the ability
+ * to perform a linear / quick sort / binary search functionality.
+**************************************************************/
 
 #include "sorting.hpp"
-#include <vector>
 
+/**************************************************************
+ *                  linearSearch()
+ * Method that allows the user to search a file and see if
+ * it conatins the number that you are looking for.
+**************************************************************/
 bool linearSearch(std::string fileName,int userSearch) {
 	std::ifstream inFile;
 	inFile.open(fileName);
 
+	// Check to see if the file has been successfully opened, if not safely exit.
 	if(inFile.fail()) {
 		std::cout << "Could not open file, an error has occurred." << std::endl;
 		exit(1);
 	}
+	// Otherwise read from the file number by number, checking it against the search item
 	else {
 		int fileInt;
 		while (inFile >> fileInt) {
+			// If found return true
 			if (fileInt == userSearch) {
 				inFile.close();
 				return true;
 			}
 		}
+		// Otherwise close the file and return false
 		inFile.close();
 		return false;
 	}
 }
 
+/**************************************************************
+ *                  quickSort()
+ * Metod that allows the user to organize an array using the
+ * quickSort sorting method, much more efficient than bubble sort
+ *
+ * Referenced website below for building of the algorithm:
+ * http://www.algolist.net/Algorithms/Sorting/Quicksort
+**************************************************************/
 void quickSort(std::vector<int> arr, int left, int right) {
 	int i = left;
 	int j = right;
@@ -55,4 +77,29 @@ void quickSort(std::vector<int> arr, int left, int right) {
 		quickSort(arr, i ,right);
 	}
 
+}
+
+/**************************************************************
+ *                  binarySearch()
+ *  This search allows us to quickly search by splitting an already
+ *  sorted menu in half and only searching half at a time, making
+ *  the overall runtime significantly shorter.
+ *
+ * Referenced website below for building of the algorithm:
+ * http://www.algolist.net/Algorithms/Binary_search
+**************************************************************/
+bool binarySearch(std::vector<int> arr, int value, int left, int right) {
+	while (left <= right) {
+		int middle = (left + right) / 2;
+		if (arr.at(middle) == value) {
+			return true;
+		}
+		else if (arr.at(middle) > value) {
+			right = middle - 1;
+		}
+		else {
+			left = middle + 1;
+		}
+	}
+	return false;
 }
